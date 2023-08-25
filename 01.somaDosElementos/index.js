c31b9589fe4cb5dc5cc41b8d076dbe318839e76f
tarcizolopes <tarcizolsj@gmail.com>
2023-08-25 09:21:54 -0300

    soma dos elementos

--- 01.somaDosElementos/index.js
+++ 01.somaDosElementos/index.js
@@ -7,27 +7,26 @@ function solucao(lista) {
       
     console.log(valorTotal); 
       
-  }
-  
-  
-  function processData(input) {
-        const strings = input.split(" ");
-      const numeros = [];
-      for(let i = 0; i < strings.length; i++) {
-            numeros.push(parseInt(strings[i] ,10));
-        }
-      solucao(numeros);
-  } 
-  
-  process.stdin.resume();
-  process.stdin.setEncoding("ascii");
-  _input = "";
-  process.stdin.on("data", function (input) {
-      _input += input;
-  });
-  
-  process.stdin.on("end", function () {
-     processData(_input);
-  });
+};
+ 
 
-  
+function processData(input) {
+      const strings = input.split(" ");
+    const numeros = [];
+    for(let i = 0; i < strings.length; i++) {
+          numeros.push(parseInt(strings[i] ,10));
+      }
+    solucao(numeros);
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+    processData(_input);
+});
+
