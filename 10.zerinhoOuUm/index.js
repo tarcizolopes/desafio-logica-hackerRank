ec38e033e37b00899564deaa360dcaea15d40263
tarcizolopes <tarcizolsj@gmail.com>
2023-08-25 09:24:36 -0300

    zerinho ou um

--- 10.zerinhoOuUm/index.js
+++ 10.zerinhoOuUm/index.js
@@ -1,36 +1,33 @@
 function solucao(jogadores) {
-    //seu codigo aqui
-    
-      const jogadaComResultadoUm = jogadores.filter((jogada) => {
-          return jogada.jogada === 1;
-      });
-      
-      const jogadaComResultadoZero = jogadores.filter((jogada) => {
-          return jogada.jogada === 0;
-      });
-      
-      if (jogadaComResultadoUm.length === 1) {
-          console.log(jogadaComResultadoUm[0].nome);
-      } else if (jogadaComResultadoZero.length === 1) {
-          console.log(jogadaComResultadoZero[0].nome);
-      } else {
-          console.log('NINGUEM')
-      };
-      
-  }
-  
-  function processData(input) {
-      solucao(JSON.parse(input));
-  } 
-  
-  process.stdin.resume();
-  process.stdin.setEncoding("ascii");
-  _input = "";
-  process.stdin.on("data", function (input) {
-      _input += input;
-  });
-  
-  process.stdin.on("end", function () {
-     processData(_input);
-  });
-  
+//seu codigo aqui
+    const jogadaComResultadoUm = jogadores.filter((jogada) => {
+        return jogada.jogada === 1;
+    });
+
+    const jogadaComResultadoZero = jogadores.filter((jogada) => {
+        return jogada.jogada === 0;
+    });
+
+    if (jogadaComResultadoUm.length === 1) {
+        console.log(jogadaComResultadoUm[0].nome);
+    } else if (jogadaComResultadoZero.length === 1) {
+        console.log(jogadaComResultadoZero[0].nome);
+    } else {
+        console.log('NINGUEM')
+    };
+};
+
+function processData(input) {
+    solucao(JSON.parse(input));
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+    processData(_input);
+});
